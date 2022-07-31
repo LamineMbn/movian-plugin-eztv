a30d14eb49a4c3020950eb4c439e3b99ae35dee6
bmlamine <lbendib.prestataire@oui.sncf>
2022-07-31 21:22:04 +0200

    Starting new version

--- eztv.js
+++ eztv.js
@@ -16,25 +16,24 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+const page = require('showtime/page');
+const service = require('showtime/service');
+const settings = require('showtime/settings');
+const http = require('showtime/http');
+const plugin = JSON.parse(Plugin.manifest);
+const logo = Plugin.path + "logo.png";
 
-var page = require('showtime/page');
-var service = require('showtime/service');
-var settings = require('showtime/settings');
-var http = require('showtime/http');
-var plugin = JSON.parse(Plugin.manifest);
-var logo = Plugin.path + "logo.png";
+const GET_TORRENTS_ENDPOINT = "api/get-torrents"
+const SEARCH_BY_NAME_ENDPOINT = "api/search"
 
 RichText = function(x) {
     this.str = x.toString();
 }
 
-RichText.prototype.toRichString = function(x) {
-    return this.str;
-}
+const blue = '6699CC', orange = 'FFA500', red = 'EE0000', green = '008B45';
 
-var blue = '6699CC', orange = 'FFA500', red = 'EE0000', green = '008B45';
 function coloredStr(str, color) {
-    return '<font color="' + color + '">' + str + '</font>';
+    return `<p style="color:${color}">${str}</p>`;
 }
 
 function setPageHeader(page, title) {
@@ -76,38 +75,60 @@ new page.Route(plugin.id + ":play:(.*):(.*):(.*):(.*):(.*)", function(page, url,
 });
 
 function bytesToSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes == 0) return '0 Byte';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-};
+}
+
+function callService(url) {
+    page.loading = true;
+    let response = http.request(url).toString();
+    page.loading = false;
+    return response
+}
+
+function retrieveAllTorrentsUrlBuilder(page, limit=10) {
+    const allTorrentsUrlWithParams = new URL(`${service.baseUrl}/${GET_TORRENTS_ENDPOINT}`);
+    allTorrentsUrlWithParams.searchParams.append("limit", limit.toString());
+    allTorrentsUrlWithParams.searchParams.append("page", page.toString());
+    return allTorrentsUrlWithParams.href
+}
+
+function retrieveSearchTorrentsUrlBuilder(query) {
+    const searchUrlWithParams = new URL(replaceSpaceByDash(query), `${service.baseUrl}/${SEARCH_BY_NAME_ENDPOINT}`);
+    return searchUrlWithParams.href
+}
+
+function replaceSpaceByDash(text) {
+    return text.replace(/%20/g, '-')
+}
 
 function browseItems(page, query) {
-    var fromPage = 1, tryToSearch = true;
+    let fromPage = 1;
+    const tryToSearch = true;
     page.entries = 0;
 
     function loader() {
         if (!tryToSearch) return false;
-        page.loading = true;
-        var url = service.baseUrl + '/api/get-torrents?limit=10&page=' + fromPage;
-        var doc = http.request(url).toString();
+        const url = retrieveAllTorrentsUrlBuilder(fromPage)
+        const response = callService(url)
+        const json = JSON.parse(response)
 
-        page.loading = false;
-        var json = JSON.parse(doc);
-        for (var i in json.torrents) {
-             var item = page.appendItem(plugin.id + ':play:' + escape(json.torrents[i].torrent_url) + ':' + escape(json.torrents[i].title) + ':' + json.torrents[i].imdb_id + ':' + json.torrents[i].season + ':' + json.torrents[i].episode, "video", {
-                 title: json.torrents[i].title,
-                 icon: json.torrents[i].small_screenshot ? 'https:' + json.torrents[i].small_screenshot : 'https://ezimg.ch/s/1/9/image-unavailable.jpg',
-                 vtype: 'tvseries',
-                 season: {number: +json.torrents[i].season},
-                 episode: {title: json.torrents[i].title, number: +json.torrents[i].episode},
-                 genre: new RichText(coloredStr('S: ', orange) + coloredStr(json.torrents[i].seeds, green) +
-                     coloredStr(' P: ', orange) + coloredStr(json.torrents[i].peers, red) +
-                     coloredStr(' Size: ', orange) + bytesToSize(json.torrents[i].size_bytes) +
-                     (json.torrents[i].imdb_id ? coloredStr('<br>IMDb ID: ', orange) + 'tt' + json.torrents[i].imdb_id : '')),
-                 tagline: new RichText(coloredStr('Released: ', orange) + new Date(json.torrents[i].date_released_unix * 1000))
-             });
-             page.entries++;
+        for (let i in json.torrents) {
+            const item = page.appendItem(plugin.id + ':play:' + escape(json.torrents[i].torrent_url) + ':' + escape(json.torrents[i].title) + ':' + json.torrents[i].imdb_id + ':' + json.torrents[i].season + ':' + json.torrents[i].episode, "video", {
+                title: json.torrents[i].title,
+                icon: json.torrents[i].small_screenshot ? 'https:' + json.torrents[i].small_screenshot : 'https://ezimg.ch/s/1/9/image-unavailable.jpg',
+                vtype: 'tvseries',
+                season: {number: +json.torrents[i].season},
+                episode: {title: json.torrents[i].title, number: +json.torrents[i].episode},
+                genre: new RichText(coloredStr('S: ', orange) + coloredStr(json.torrents[i].seeds, green) +
+                    coloredStr(' P: ', orange) + coloredStr(json.torrents[i].peers, red) +
+                    coloredStr(' Size: ', orange) + bytesToSize(json.torrents[i].size_bytes) +
+                    (json.torrents[i].imdb_id ? coloredStr('<br>IMDb ID: ', orange) + 'tt' + json.torrents[i].imdb_id : '')),
+                tagline: new RichText(coloredStr('Released: ', orange) + new Date(json.torrents[i].date_released_unix * 1000))
+            });
+            page.entries++;
              if (service.enableMetadata) {
                  item.bindVideoMetadata({
                      imdb: 'tt' + json.torrents[i].imdb_id
@@ -119,7 +140,6 @@ function browseItems(page, query) {
     }
     loader();
     page.paginator = loader;
-    page.loading = false;
 }
 	
 new page.Route(plugin.id + ":start", function(page) {
@@ -128,34 +148,34 @@ new page.Route(plugin.id + ":start", function(page) {
         title: 'Search at ' + service.baseUrl
     });
     browseItems(page);
-    page.loading = false;
 });
 
 function search(page, query) {
     setPageHeader(page, plugin.title);
     page.entries = 0;
     page.loading = true;
-    var doc = http.request(service.baseUrl + "/search/"+ escape(query).replace(/%20/g, '-')).toString();
+    const url = retrieveSearchTorrentsUrlBuilder(query)
+    const response = callService(url)
     // 1-link to the show, 2-show's title, 3-episode url, 4-episode's title, 5-magnet&torrent urls, 6-size, 7-released, 8-seeds
-    var re = /<tr name="hover"[\s\S]*?<a href="([\s\S]*?)"[\s\S]*?alt="Info" title="([\s\S]*?)"[\s\S]*?<a href="([\s\S]*?)"[\s\S]*?class="epinfo">([\s\S]*?)<\/a>[\s\S]*?<td align="center"([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">[\s\S]*?">([\s\S]*?)</g;
-    var match = re.exec(doc);
+    const re = /<tr name="hover"[\s\S]*?<a href="([\s\S]*?)"[\s\S]*?alt="Info" title="([\s\S]*?)"[\s\S]*?<a href="([\s\S]*?)"[\s\S]*?class="epinfo">([\s\S]*?)<\/a>[\s\S]*?<td align="center"([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">([\s\S]*?)<\/td>[\s\S]*?class="forum_thread_post">[\s\S]*?">([\s\S]*?)</g;
+    let match = re.exec(response);
     while (match) {
-        var re2 = /<a href="([\s\S]*?)"/g;
-        var urls = re2.exec(match[5]);
-        var lnk = '';        
+        const re2 = /<a href="([\s\S]*?)"/g;
+        let urls = re2.exec(match[5]);
+        let lnk = '';
         while (urls) { // we prefer .torrent 
             lnk = urls[1];
             urls = re2.exec(match[5])
         }
-        var item = page.appendItem('torrent:video:' + lnk, "video", {
-             title: new RichText(match[4]),
-             icon: logo,
-             genre: new RichText((match[8] ? coloredStr('Seeds: ', orange) + coloredStr(match[8], green) + ' ' : '') +
-                 coloredStr('Size: ', orange) + match[6]),
-             tagline: new RichText(coloredStr('<br>Released: ', orange) + match[7])
-             });
-             page.entries++;
-      match = re.exec(doc);
+        const item = page.appendItem('torrent:video:' + lnk, "video", {
+            title: new RichText(match[4]),
+            icon: logo,
+            genre: new RichText((match[8] ? coloredStr('Seeds: ', orange) + coloredStr(match[8], green) + ' ' : '') +
+                coloredStr('Size: ', orange) + match[6]),
+            tagline: new RichText(coloredStr('<br>Released: ', orange) + match[7])
+        });
+        page.entries++;
+      match = re.exec(response);
     }
     page.loading = false;
 }
